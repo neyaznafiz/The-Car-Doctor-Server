f17c6ab6343468281314d8065f44a2a44683691d
niiyazobug <niiyaz5555@gmail.com>
2022-04-22 11:53:01 +0600

    mongodb connecter

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // port
 const port = process.env.PORT || 5000
@@ -9,6 +10,18 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+// 
+const uri = `mongodb+srv://${process.env.TCD_USER}:${process.env.TCD_PASS}@cluster0.l30j2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Genius car db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 //
