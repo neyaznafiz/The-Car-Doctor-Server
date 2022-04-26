251137ba7a10864c4735ba05b76f16a139e6fccf
niiyazobug <niiyaz5555@gmail.com>
2022-04-26 22:10:38 +0600

    steps added

---
+++ __server_deploy.js
@@ -0,0 +1,13 @@
+/**
+ * ------------------------
+ * this is for one time for your computer
+ * ------------------------
+ * 1. create heroku account.
+ * 2. verify email
+ * 3. install cli
+ * 4. heroku login
+ * -----------------------------
+ * for each project
+ * ---------------------------
+ * 1. heroku create project
+*/
